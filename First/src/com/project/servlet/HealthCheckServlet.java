608f5f6d7f7f1ea3d9932ccc204062d13d05999e
Anil <thota.anil19@gmail.com>
2019-03-13 11:10:10 -0700

    'samplehealthcheckservlet'

---
+++ First/src/com/project/servlet/HealthCheckServlet.java
@@ -0,0 +1,28 @@
+package com.project.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// Extend HttpServlet class
+public class HealthCheckServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -5713124872543112242L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// Set response content type
+		response.setContentType("text/html");
+
+		// Actual logic goes here.
+		PrintWriter out = response.getWriter();
+		out.println("<h1> Http Status: 200 </h1>");
+	}
+
+	
+}
